e5fd8fe40d1ef9a39c1e7b2d99c595b86ff46d84
Bhanu Pratap <techieonmail@gmail.com>
2022-01-12 01:36:23 +0530

    codeforces python solutions

--- Hungry Sequence.py
+++ Hungry Sequence.py
@@ -1,7 +1,16 @@
-n = input()
+n = int(input())
 
-for i in range(2, (10**7)+1):
-    if n > 0:
-        n = n-1
-    else:
+isPrimes = [True] * ((10**6)+1)
+isPrimes[1] = False
+
+count = 0
+
+for i in range(2, 10**3):
+    if count > n-1:
         break
+    else:
+        if isPrimes[i]:
+            print(i, end=" ")
+            count += 1
+            for j in range(i, 10**3):
+                isPrimes[i*j] = False
