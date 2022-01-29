50474526dd76c6dc1c5c6f1105d97c9ea787e917
Bhanu Pratap <techieonmail@gmail.com>
2022-01-29 23:34:06 +0530

    codeforces python solution

--- Flipping Game 327A.py
+++ Flipping Game 327A.py
@@ -1,24 +1,36 @@
 """
-we can find the indexes of 1s
-(the maximum difference between two adjustend indexes +1) = ans
+Largest Sum Contiguous Subarray problem
+similar approach to "subsequence of maximal sum"
+better known as "Kadane's algorithm"
+but more suitable approach to this problem is 
+"Max Sum in Contiguous Subarray" or "Largest Sum Contiguous Subarray"
+algorithm can be found on GFG
 """
 
 n = int(input())
 arr = [int(i) for i in input().split()]
-ones = []
-maximum_diff = 0
+S = 0
 
 for i in range(n):
-    if arr[i] == 1:
-        ones.append(i)
-    
-for i in range(len(ones)):
-        maximum_diff = max(maximum_diff, ones[i]-ones[i-1])
+    if arr[i] == 0:
+        arr[i] = 1
+    else:
+        S += 1
+        arr[i] = -1
 
+max_so_far = S-1
+max_ending_here = S
 
-if len(ones) == len(arr):
-    print(0)
-elif maximum_diff == 1 or maximum_diff == 0:
-    print(len(arr))
+for i in range(n):
+    max_ending_here = max_ending_here + arr[i]
+
+    if max_ending_here < S:
+        max_ending_here = S
+
+    elif max_ending_here > max_so_far:
+        max_so_far = max_ending_here
+
+if S != 1:
+    print(max_so_far)
 else:
-    print(maximum_diff+1)
+    print(0)
