9b8cd6c8e8961e9e6d4850140e9074e70c8f3322
Bhanu Pratap <techieonmail@gmail.com>
2022-02-01 23:21:36 +0530

    codeforces python solutions

--- Polo the Penguin and Matrix 289B.py
+++ Polo the Penguin and Matrix 289B.py
@@ -1,13 +1,35 @@
+# Step 1: checking if all elements have same mod values with d
+#         a[i] % d should be equal for all
+# Step 2: Sort array
+#         Minimum will be when we make all elements equals to middle element
+
 n, m, d = map(int, input().split())
 
-matrix = [[int(j) for j in input().split()] for _ in range(n)]
+arr = []
+remainder = -1
+flag = True
 
-mid = matrix[n//2][m//2]
-sum = 0
-for i in range(n):
-    for j in range(m):
-        if matrix[i][j] != mid:
-            sum += mid % matrix[i][j] + d
-            print(sum)
+for _ in range(n):
+    for i in input().split():
+        arr.append(int(i))
+        temp = int(i) % d
+        if remainder == -1:
+            remainder = temp
+        elif remainder != -1 and temp != remainder:
+            flag = False
+            break
+    if flag == False:
+        break
 
-print(sum)
+if flag:
+    arr.sort()
+    k = n*m
+    sum_mod = 0
+    mid = arr[int(k/2)]
+    for i in range(k):
+        sum_mod += abs(arr[i] - mid)
+
+if flag:
+    print(int(sum_mod/d))
+else:
+    print(-1)
